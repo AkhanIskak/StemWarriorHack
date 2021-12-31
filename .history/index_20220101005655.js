ec6866e5a88bc8506bf5c7dabee5809463f52ca2
iskak akhan <alex12hitman@gmail.com>
2022-01-01 03:19:52 +0600

    .history removal

--- .history/index_20220101005655.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const dotenv = require('dotenv');
-dotenv.config
-
-app.use(express.json());
-
-app.listen()
--- .history/index_20220101005913.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const dotenv = require('dotenv');
-dotenv.config({path: "./configuration.env"});
-app.use(express.json());
-
-
-app.
-app.listen(process.env.PORT,()=>console.log(`server is listening on port ${process.env.PORT}`))
